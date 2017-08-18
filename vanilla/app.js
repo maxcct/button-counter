75a19f8206a28ab758f46164c9a20aca3764ec18
MaxCCT <maxcct@cantab.net>
2017-08-18 13:09:31 +0100

    Moves function from controller to view

--- vanilla/app.js
+++ vanilla/app.js
@@ -6,10 +6,23 @@ var controller = {
 	    	alert('Giving up :( Cannot create an XMLHTTP instance');
 	    	return false;
 	    }
-	    httpRequest.onreadystatechange = this.buttons.createButtons;
+	    httpRequest.onreadystatechange = this.handleRequest;
 	    httpRequest.open('GET', 'data.json');
 	    httpRequest.send();
 	},
+
+	handleRequest: function() {
+    	if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    		if (httpRequest.status === 200) {
+    			var response = JSON.parse(httpRequest.responseText);
+    			var players = response.players;
+				view.init(response, players);
+      		} else {
+        		alert('There was a problem with the request.');
+      		}
+    	}
+	},
+
     counter: {
         count: 0,
         changeHandler: function(change) {
@@ -17,28 +30,8 @@ var controller = {
             view.updateCounter();
         }
     },
+
     buttons: {
-    	buttonsHTML: [],
-
-		createButtons: function() {
-	    	if (httpRequest.readyState === XMLHttpRequest.DONE) {
-	    		if (httpRequest.status === 200) {
-	    			var response = JSON.parse(httpRequest.responseText);
-	    			var players = response.players;
-					for(var i = 0; i < response.total; i++) {
-					    var buttonHTML = "<button onclick='controller.buttons.clickHandler(this)'"
-					    buttonHTML += " id='" + i + "'>";   
-					    buttonHTML += players[i].price + ": ";
-					    buttonHTML += players[i].player + "</button>";
-					    controller.buttons.buttonsHTML.push(buttonHTML);
-					}
-					view.init();
-	      		} else {
-	        		alert('There was a problem with the request.');
-	      		}
-	    	}
-		},
-
     	pressed: [],
 
     	clickHandler: function(button) {
@@ -55,6 +48,7 @@ var controller = {
         	controller.counter.changeHandler(change);
     	}
     },
+
 	init: function() {
 		this.makeRequest();
 	}
@@ -63,14 +57,27 @@ var controller = {
 controller.init();
 
 var view = {
-	init: function() {
+	init: function(response, players) {
 		buttonsDiv = document.getElementById("buttons");
-		buttonsHTML = controller.buttons.buttonsHTML;
+		buttonsHTML = createButtons(response, players);
 		buttonsHTML.forEach(function(button) {
 			buttonsDiv.innerHTML += button;
 		});
 		this.updateCounter();
 	},
+
+	createButtons: function() {
+		buttonsHTML = [];
+		for(var i = 0; i < response.total; i++) {
+			var buttonHTML = "<button onclick='controller.buttons.clickHandler(this)'";
+			buttonHTML += " id='" + i + "'>";   
+			buttonHTML += players[i].price + ": ";
+			buttonHTML += players[i].player + "</button>";
+			buttonsHTML.push(buttonHTML);
+		}
+		return buttonsHTML;
+	},
+
 	updateCounter: function() {
 		document.getElementById("counter").innerHTML = controller.counter.count;
 	}
